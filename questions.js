7e0659ae40d3a84d84f2083edc919a0b05c2cced
unknown <catcorbin0919@gmail.com>
2023-10-21 19:08:18 -0600

    fine tuned javascript

---
+++ questions.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: 'Commonly used data types do NOT include:',
+        answers: [
+            { text: 'strings', correct: false },
+            { text: 'booleans', correct: false },
+            { text: 'alerts', correct: true },
+            { text: 'numbers', correct: false }
+        ]
+    },
+    {
+        question: 'A condition in an if/else statement is enclosed with:',
+        answers: [
+            { text: 'parentheses', correct: true },
+            { text: 'curly brackets', correct: false },
+            { text: 'quotes', correct: false },
+            { text: 'square brackets', correct: false }
+        ]
+    },
+    {
+        question: 'Arrays in JavaScript can be used to store:',
+        answers: [
+            { text: 'numbers and strings', correct: false },
+            { text: 'other arrays', correct: false },
+            { text: 'booleans', correct: false },
+            { text: 'all of the above', correct: true }
+        ]
+    },
+    {
+        question: 'String values must be enclosed within _____ when being assigned to variables:',
+        answers: [
+            { text: 'commas', correct: false },
+            { text: 'curly brackets', correct: false },
+            { text: 'quotes', correct: true },
+            { text: 'parentheses', correct: false }
+        ]
+    },
+];
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const startButton = document.getElementById('start-btn');
     const questionContainer = document.getElementById('question-container');
     const endContainer = document.getElementById('end-container');
@@ -7,21 +7,33 @@ document.addEventListener('DOMContentLoaded', function() {
     const initialsInput = document.getElementById('initials');
     const submitButton = document.getElementById('submit-btn');
     const finalScoreDisplay = document.getElementById('final-score');
+    var timer = document.getElementById('timer');
+    var message = document.getElementById('message');
 
     let currentQuestionIndex = 0;
-    //timer starts countdown at 60 seconds
-    let time = 60;
-
+    //timer starts countdown at 75 seconds
+    let time = 75;
+    let setIntervalId = ''
+    let scoresList = JSON.parse(localStorage.getItem("scoresList")) || []
     startButton.addEventListener('click', startQuiz);
 
+    //when the "Lets code!"" button is pushed, the questions appear and the timer starts
     function startQuiz() {
         startButton.classList.add('hide');
         questionContainer.classList.remove('hide');
+        setIntervalId = setInterval(countdown, 1000);
         setNextQuestion();
     }
-
+    function countdown() {
+        if (time < 0) {
+            clearInterval(setIntervalId)
+        } else {
+            timer.textContent = time--
+        }
+    }
     function setNextQuestion() {
-        resetState();
+        message.textContent = ""
+        //resetState();
         showQuestion(questions[currentQuestionIndex]);
     }
 
@@ -30,27 +42,32 @@ document.addEventListener('DOMContentLoaded', function() {
         question.answers.forEach((answer, index) => {
             answerButtons[index].innerText = answer.text;
             answerButtons[index].addEventListener('click', () => {
-                if (index === question.correct) {
+                if (question.answers[index].correct) {
+                    message.textContent = "Correct!"
                     //if answer is correct, move to next question
                 } else {
                     //if the answer is incorrect, subtract 3 extra seconds from timer
-                    time -= 3; 
+                    time -= 3;
+                    message.textContent = "Wrong!"
                 }
-                currentQuestionIndex++;
-                if (currentQuestionIndex < questions.length) {
-                    setNextQuestion();
-                } else {
-                    endQuiz();
-                }
+                setTimeout(() => {
+                    currentQuestionIndex++;
+                    if (currentQuestionIndex < questions.length) {
+                        setNextQuestion();
+                    } else {
+                        endQuiz();
+                    }
+                }, 500)
             });
         });
     }
 
     function endQuiz() {
+        clearInterval(setIntervalId);
         questionContainer.classList.add('hide');
         endContainer.classList.remove('hide');
         //displays the final score
-        finalScoreDisplay.innerText = time;
+        finalScoreDisplay.innerText = timer.textContent;
     }
 
     function resetState() {
@@ -58,43 +75,12 @@ document.addEventListener('DOMContentLoaded', function() {
             answerButtons[0].classList.remove('hide');
         }
     }
+    submitButton.addEventListener("click", function () {
+        let user = {
+            initial: initialsInput.value,
+            score: timer.textContent
+        }
+        scoresList.push(user)
+        localStorage.setItem("scoresList", JSON.stringify(scoresList))
+    })
 });
-
-const questions = [
-    {
-        question: 'Commonly used data types do NOT include:',
-        answers: [
-            { text: 'strings', correct: false },
-            { text: 'booleans', correct: false },
-            { text: 'alerts', correct: true },
-            { text: 'numbers', correct: false }
-        ]
-    },
-    {
-        question: 'A condition in an if/else statement is enclosed with:',
-        answers: [
-            { text: 'parentheses', correct: true },
-            { text: 'curly brackets', correct: false },
-            { text: 'quotes', correct: false },
-            { text: 'square brackets', correct: false }
-        ]
-    },
-    {
-        question: 'Arrays in JavaScript can be used to store:',
-        answers: [
-            { text: 'numbers and strings', correct: false },
-            { text: 'other arrays', correct: false },
-            { text: 'booleans', correct: false },
-            { text: 'all of the above', correct: true }
-        ]
-    },
-    {
-        question: 'String values must be enclosed within _____ when being assigned to variables:',
-        answers: [
-            { text: 'commas', correct: false },
-            { text: 'curly brackets', correct: false },
-            { text: 'quotes', correct: true },
-            { text: 'parentheses', correct: false }
-        ]
-    },
-];
